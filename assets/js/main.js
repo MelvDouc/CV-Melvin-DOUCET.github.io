5b0c2568b137c2032a344892bb248d22ed6bc953
MelvDouc <melv.douc@gmail.com>
2021-02-14 22:49:41 +0100

    Correction de fautes de frappe

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 // [0] Variables et fonctions
 
 const petitePhoto = document.querySelector('#petite-photo'),
-grandePhoto = document.querySelector('#grande-photo'),
-h2s = document.getElementsByTagName('h2'),
-h3sCompetences = document.querySelectorAll('.competence h3'),
-formation = document.querySelectorAll('#formation > ul.deux-col > li'),
-experience = document.querySelectorAll('#experience > ul > li');
+    grandePhoto = document.querySelector('#grande-photo'),
+    h2s = document.getElementsByTagName('h2'),
+    h3sCompetences = document.querySelectorAll('.competence h3'),
+    formation = document.querySelectorAll('#formation > ul.deux-col > li'),
+    experience = document.querySelectorAll('#experience > ul > li');
 
 function ajouterClasses(elements, ...classesBootstrap) {
-    for(element of elements) {
+    for (element of elements) {
         element.classList.add(...classesBootstrap);
-    }  
+    }
 }
 
 const toggleClass = (element, class1) => element.classList.toggle(class1);
@@ -23,30 +23,30 @@ ajouterClasses(experience, 'mb-3');
 
 // [2] Animation de la photo
 
-petitePhoto.addEventListener('mouseover', function() {
+petitePhoto.addEventListener('mouseover', function () {
     this.style.display = 'none';
     grandePhoto.style.display = 'block';
 });
-grandePhoto.addEventListener('mouseout', function() {
+grandePhoto.addEventListener('mouseout', function () {
     petitePhoto.style.display = 'block';
     this.style.display = 'none';
 });
 
 // [3] Animation des sections et sous-sections
 
-for(h2 of h2s) {
-    if(h2.classList.contains('titre-inactif')) {
-        for(child of h2.parentElement.children) {
-            if(!child.matches('h2')) {
+for (h2 of h2s) {
+    if (h2.classList.contains('titre-inactif')) {
+        for (child of h2.parentElement.children) {
+            if (!child.matches('h2')) {
                 toggleClass(child, 'section-inactive');
             }
         }
     };
-    h2.addEventListener('click', function() {
+    h2.addEventListener('click', function () {
         toggleClass(this, 'titre-inactif');
-        for(child of this.parentElement.children) {
-            if(!child.matches('h2')) {
-                if(window.getComputedStyle(child).getPropertyValue('display') != 'none') {
+        for (child of this.parentElement.children) {
+            if (!child.matches('h2')) {
+                if (window.getComputedStyle(child).getPropertyValue('display') != 'none') {
                     child.classList.add('fade-out');
                     child.classList.remove('fade-in');
                     setTimeout(toggleClass, 500, child, 'section-inactive');
@@ -58,24 +58,24 @@ for(h2 of h2s) {
             }
         }
     });
-    h2.addEventListener('mouseover', function() {
-        this.title = (this.classList.contains('titre-inactif')) ? 'Cliquez pour dérouler.' : 'Cliquez pour replier';
+    h2.addEventListener('mouseover', function () {
+        this.title = (this.classList.contains('titre-inactif')) ? 'Cliquez pour dérouler.' : 'Cliquez pour replier.';
     })
 };
 
-for(h3 of h3sCompetences) {
-    if(h3.classList.contains('titre-inactif')) {
-        for(child of h3.parentElement.children) {
-            if(!child.matches('h3')) {
+for (h3 of h3sCompetences) {
+    if (h3.classList.contains('titre-inactif')) {
+        for (child of h3.parentElement.children) {
+            if (!child.matches('h3')) {
                 toggleClass(child, 'section-inactive');
             }
         }
     };
-    h3.addEventListener('click', function() {
+    h3.addEventListener('click', function () {
         toggleClass(this, 'titre-inactif');
-        for(child of this.parentElement.children) {
-            if(!child.matches('h3')) {
-                if(window.getComputedStyle(child).getPropertyValue('display') != 'none') {
+        for (child of this.parentElement.children) {
+            if (!child.matches('h3')) {
+                if (window.getComputedStyle(child).getPropertyValue('display') != 'none') {
                     child.classList.add('fade-out');
                     child.classList.remove('fade-in');
                     setTimeout(toggleClass, 500, child, 'section-inactive');
